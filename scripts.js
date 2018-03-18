486de5195e5fe58c47a0019e4df7f5afec7b8d03
Peter Dahlén <peterjandahlen@gmail.com>
2018-03-18 12:57:43 +0100

    function updateButton

--- scripts.js
+++ scripts.js
@@ -13,3 +13,13 @@ function togglePlay() {
         video.pause();
     }
 }
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
